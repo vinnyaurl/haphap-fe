41190e3ad4dfd3740144c81e2348bb70c8813e03
vinnyaurl <vinnyaureliya14@gmail.com>
2026-05-10 19:29:24 +0700

    created folder structure and added app_colors.dart

---
+++ lib/core/constants/theme/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFFF6751E);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color background = Color(0xFFF8F8F8);
+  static const Color greyLight = Color(0xFFBBBBBB);
+  static const Color greyDark = Color(0xFF494B4B);
+  static const Color black = Color(0xFF222222);
+}
