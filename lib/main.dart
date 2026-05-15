694834e085f699b067d84cd0ee47d7896051cdf3
vinnyaurl <vinnyaureliya14@gmail.com>
2026-05-15 14:56:50 +0700

    updated beranda.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:haphap_fe/core/theme/app_colors.dart';
 import 'package:haphap_fe/presentation/pages/splash/splash_screen.dart';
 import 'package:haphap_fe/presentation/pages/customer/beranda.dart';
+import 'package:google_fonts/google_fonts.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -14,7 +15,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'HapHap',
-      theme: ThemeData(useMaterial3: true, colorSchemeSeed: AppColors.primary),
+      theme: ThemeData(
+        useMaterial3: true, 
+        textTheme: GoogleFonts.plusJakartaSansTextTheme(
+          Theme.of(context).textTheme,
+        ),
+        colorSchemeSeed: AppColors.primary),
       // home: const SplashScreen(),
       home: const BerandaPage(),
     );
