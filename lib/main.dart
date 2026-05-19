38c03bf1fa67fe6fa70d9ef195473c5c99c1d063
lucky andreas <luckyandreas224@gmail.com>
2026-05-19 22:27:48 +0700

    chore: test

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:haphap_fe/core/theme/app_colors.dart';
-import 'package:haphap_fe/presentation/pages/splash/splash_screen.dart';
 import 'package:haphap_fe/presentation/pages/customer/beranda.dart';
 import 'package:google_fonts/google_fonts.dart';
 void main() {
