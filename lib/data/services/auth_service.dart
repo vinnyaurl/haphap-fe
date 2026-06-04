1397d94e1bebc02fa01dfa5b5ad7f5fd26f26479
lucky andreas <luckyandreas224@gmail.com>
2026-06-05 00:46:19 +0700

    feat: login backend integration and google sign in setup

--- lib/data/services/auth_service.dart
+++ lib/data/services/auth_service.dart
@@ -1,9 +1,9 @@
 import 'package:haphap_fe/core/network/api_client.dart';
 import 'package:haphap_fe/data/models/auth_response_model.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthService {
-  /// POST /api/auth/login
-  /// Body: { email, password }
   static Future<AuthResponse> login({
     required String email,
     required String password,
@@ -15,8 +15,6 @@ class AuthService {
     return AuthResponse.fromJson(json);
   }
 
-  /// POST /api/auth/register
-  /// Body: { name, email, password, phone }
   static Future<AuthResponse> register({
     required String name,
     required String email,
@@ -31,4 +29,21 @@ class AuthService {
     });
     return AuthResponse.fromJson(json);
   }
+
+  Future<String?> signInWithGoogle() async {
+      try {
+        final webClientId = dotenv.env['GOOGLE_WEB_CLIENT_ID'];
+        final googleSignIn = GoogleSignIn(
+          serverClientId: webClientId,
+        );
+        await googleSignIn.signOut();
+        final googleUser = await googleSignIn.signIn();
+        if (googleUser == null) return null;
+        final googleAuth = await googleUser.authentication;
+        return googleAuth.idToken; 
+      } catch (e) {
+        print('🔥 Google Sign-In Error: $e');
+        return null; 
+      }
+    }
 }
