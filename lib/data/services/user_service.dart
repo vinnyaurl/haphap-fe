1b916eea35f07a9922e2db71658e04d813d0332c
vinnyaurl <vinnyaureliya14@gmail.com>
2026-06-06 14:46:05 +0700

    feat: integrated akun.dart to backend

---
+++ lib/data/services/user_service.dart
@@ -0,0 +1,16 @@
+import 'package:haphap_fe/core/network/api_client.dart';
+import 'package:haphap_fe/data/models/user_profile_model.dart';
+
+class UserService {
+  UserService._();
+
+  static Future<UserProfileModel> getMe() async {
+    try {
+      final json = await ApiClient.get('/users/me'); 
+      
+      return UserProfileModel.fromJson(json['data'] ?? json);
+    } catch (e) {
+      throw Exception('Gagal mengambil data profil: $e');
+    }
+  }
+}
